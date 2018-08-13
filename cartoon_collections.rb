d975a2653e9d0e3317abdc254eafe8e604ff38f2
Ricky Liu <rorix12321@gmail.com>
2018-08-13 09:56:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   maybecheese = []
   snacks.find do |x|
-    if x == "#{cheese_types}"
+    snacks.include?(cheese_types)
       maybecheese << x
     end
   end
